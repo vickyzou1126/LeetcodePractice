114d061231af21a67ab11a4cc2b67ad370b886bd
Wan Zou <wanzou1126@gmail.com>
2020-03-16 10:09:24 +1100

    add new function longestCommonPrefix into Solution

--- Solution.java
+++ Solution.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 class Solution {
 
     public boolean isPalindrome(int x) {
@@ -16,4 +20,39 @@ class Solution {
 		 }
 		 return true;
 	 }
+    
+    /*
+     Write a function to find the longest common prefix string amongst an array of strings.
+
+	 If there is no common prefix, return an empty string "".
+     */
+    public  String longestCommonPrefix(String[] strs) {
+		if(strs.length == 0 ) return "";
+		if(strs.length == 1 ) return strs[0];
+
+		List<String> list = Arrays.asList(strs);
+		list.sort(Comparator.comparingInt(String::length));
+		String str = list.get(0);
+		
+		String longestCommonPrefix = "";
+		
+		for(int i=0;i<str.length();i++){
+			for(int j=str.length();j>i;j--){
+				String s =str.substring(i, j);
+				if(Contains(list,s) && longestCommonPrefix.length()<s.length()){
+					longestCommonPrefix = s;
+					if(longestCommonPrefix.length() == str.length()) return longestCommonPrefix;
+				}
+			}
+		}
+        return longestCommonPrefix;
+    }
+	private  boolean Contains(List<String> list,String s){
+		for(String str:list){
+			if(!str.startsWith(s)) return false;
+		}
+		return true;
+	}
+
 }
+
