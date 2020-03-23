a40b778fbc9cd58d05421e56b2db9f0e514ce4cb
Wan Zou <wanzou1126@gmail.com>
2020-03-23 20:46:35 +1100

    added threeSumClosest

--- Practice.kt
+++ Practice.kt
@@ -1,11 +1,12 @@
 import kotlin.math.abs
 
-
 fun main(args: Array<String>) {
-    var nums:IntArray= intArrayOf(-1,0,1,0)
-    println(threeSum(nums))
-
+    var nums:IntArray= intArrayOf(1,2,5,10,11)
+    println(threeSumClosest(nums,12))
 }
+/*
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ */
 fun threeSum(nums: IntArray): List<List<Int>> {
     var res:MutableList<List<Int>> = emptyList<List<Int>>().toMutableList()
     var len = nums.size
@@ -58,4 +59,32 @@ fun threeSum(nums: IntArray): List<List<Int>> {
     }
 
     return res.distinct()
+}
+
+/*
+Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+ */
+fun threeSumClosest(nums: IntArray, target: Int): Int {
+    var len = nums.size
+    if(len<=3)
+        return nums.sum()
+    nums.sort()
+    var sum:Int=Int.MAX_VALUE
+    var res=Int.MAX_VALUE
+    for (i in 0 .. len-3){
+        loop@ for(j in i+1 .. len-2) {
+            for (k in j + 1 .. len - 1){
+                var tempsum=nums.get(i)+nums.get(j)+nums.get(k)
+                var temp =abs(tempsum-target)
+                if(temp<sum){
+                    sum=temp;
+                    res=tempsum
+                }else if(tempsum > target){
+                    continue@loop
+                }
+                if(temp==0) return target
+            }
+        }
+    }
+    return res
 }
